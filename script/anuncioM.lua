40af3cadee675e411acc094b86867ee703a4c9dd
marcella3 <marcela3llanos@gmail.com>
2017-09-19 01:37:31 +0400

    subida proyecto a github avances en las conecciones entre primera pantalla y segunda pantalla

--- script/anuncioM.lua
+++ script/anuncioM.lua
@@ -1,4 +1,18 @@
-font-family = 'Arial'
+font-family = 'Serif'
 font-size = 1.10em
-font-weight = 35
+font-weight = 300
 font-color = black
+
+function handler(e)
+	if (e.class == 'ncl' and e.type == 'presentation' and e.action=='start') then
+		print("PULSE EL BOTON ROJO DEL CONTROL") io.flush()
+		exibeTexto("PULSE EL BOTON ROJO DEL CONTROL")
+	end
+end
+function exibeTexto(texto)
+	canvas:attrFont(font-family, font-size, font-weight)
+	canvas:attrColor(font-color)
+	canvas:drawText(0,0, texto)
+	canvas:flush()
+end
+event.register(handler)
